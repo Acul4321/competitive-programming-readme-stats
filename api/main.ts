1e7e6dfcdaec6b648b08b891fc166f715bc1add2
Luca Rogers <lucarogers666@gmail.com>
2025-01-07 17:37:23 +0000

    api init

--- api/main.ts
+++ api/main.ts
@@ -1,8 +1,21 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Application, Router } from "@oak/oak";
+import { oakCors } from "@tajpouria/cors";
+
+const PORT:string = Deno.env.get("SERVER_PORT") ?? "8000";
+const app = new Application();
+const router = new Router();
+
+app.use(oakCors());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  console.log("Listening on http://localhost:" + PORT + '/')
 }
+
+router.get("/", (ctx) => {
+  ctx.response.body = 'Hello World'
+})
+
+await app.listen({ port: parseInt(PORT) });
