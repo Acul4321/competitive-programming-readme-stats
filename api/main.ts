ce2c21944b1eb6db75397fe322d37a18452ee4cb
Luca Rogers <lucarogers666@gmail.com>
2025-01-10 02:18:51 +0000

    main url query working

--- api/main.ts
+++ api/main.ts
@@ -2,20 +2,55 @@ import { Application, Router } from "@oak/oak";
 import { oakCors } from "@tajpouria/cors";
 
 const PORT:string = Deno.env.get("SERVER_PORT") ?? "8000";
-const app = new Application();
 const router = new Router();
 
+router.get("/", (ctx) => {
+  ctx.response.body = 'Welcome to The Competitive Programming Readme Stats \nThe Supported Platforms Include: \natcoder \n \nThe Types of Cards are:\nstats \n \nThe Command Structure is:\n/{platform}/{type}/{username}?{optionalPeram}'
+})
+
+router.get("/:platform/:type/:username", (ctx) => {
+  //validate platform
+  switch(ctx.params.platform) {
+    case "atcoder": {
+      ctx.response.body = "atcoder";
+      break;
+    }
+    default: {
+      ctx.response.body = "please provide a supported platform";
+      return;
+    }
+  }
+  //validate type
+  switch(ctx.params.type) {
+    case "stats": {
+      ctx.response.body = "stats";
+      break;
+    }
+    default: {
+      ctx.response.body = "please provide a correct card type";
+      return;
+    }
+  }
+  //validate username
+
+  // parse optional perameters
+  const queryParam = new Map<string, string>();
+  ctx.request.url.searchParams.forEach((v, k) => {
+    queryParam.set(k, v);
+  });
+
+  
+  ctx.response.body = Object.fromEntries(queryParam);
+  
+  //loop through the optional query parameters
+  for (const key of queryParam.keys()) {
+    console.log(`Key: ${key}, Value: ${queryParam.get(key)}`);
+  }
+})
+
+const app = new Application();
 app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
-
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Listening on http://localhost:" + PORT + '/')
-}
-
-router.get("/", (ctx) => {
-  ctx.response.body = 'Hello World'
-})
 
 await app.listen({ port: parseInt(PORT) });
